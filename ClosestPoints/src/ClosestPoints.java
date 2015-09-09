ed0fbb3ca2b50bad2ab9da520362b7a72b4c8e53
René Anda Nielsen <anda83@gmail.com>
2015-09-09 14:24:21 +0200

    Pair class added

--- ClosestPoints/src/ClosestPoints.java
+++ ClosestPoints/src/ClosestPoints.java
@@ -2,6 +2,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -13,12 +14,12 @@ public class ClosestPoints {
 
     static ArrayList<Point> points = new ArrayList();
 
-    public static void main(String[] args) throws FileNotFoundException{
-        File file = new File("d198-tsp.txt");
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("a280-tsp.txt");
         Scanner sc = new Scanner(file); // (System.in);
         int id = 1;
-        while (sc.hasNextLine()){
-            String[] fields = sc.nextLine().trim().replaceAll("\\s+"," ").split(" ");
+        while (sc.hasNextLine()) {
+            String[] fields = sc.nextLine().trim().replaceAll("\\s+", " ").split(" ");
             if (fields[0].compareTo(id + "") == 0) {
                 double x = Double.parseDouble(fields[1]);
                 double y = Double.parseDouble(fields[2]);
@@ -27,18 +28,58 @@ public class ClosestPoints {
                 id++;
             }
         }
+        Collections.sort(points);
+        closestPair(points);
     }
 
-    private static class Point{
+    public static void closestPair(ArrayList<Point> list) {
+        
+ 
+        
 
-        final int id;
-        final double x, y;
+    }
+
+    
+    private static class Point implements Comparable {
+
+        int id;
+        double x;
+        double y;
 
         public Point(int id, double x, double y) {
             this.id = id;
             this.x = x;
             this.y = y;
         }
-    }
 
+        public Double distance(Point p) {
+            Double distance = Math.sqrt((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y));
+            return distance;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            if (this.x < ((Point) o).x) {
+                return -1;
+            }
+            if (this.x > ((Point) o).y) {
+                return 1;
+            }
+            return 0;
+        }
+    }
+    
+    private static class Pair {
+        Point p1;
+        Point p2;
+        
+        public Pair(Point p1, Point p2) {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+        
+        public Double distance() {
+            return p1.distance(p2);
+        }
+    }
 }
